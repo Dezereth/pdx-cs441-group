4593068a5826c9176c59cfa9f21081e2a0871f33
Ray Emory <remory@summitsemi.com>
2019-06-04 16:59:31 -0700

    pulled changes from master

--- montecarlo.py
+++ montecarlo.py
@@ -3,10 +3,12 @@ import chess
 import numpy as np
 import random
 import time
+from operator import attrgetter
+from matplotlib import pyplot as plt
 
 #implementation based on Pseuocode at https://www.geeksforgeeks.org/ml-monte-carlo-tree-search-mcts/
 class node:
-    def __init__(self, state, parent, root=False, color=None):
+    def __init__(self, state, parent, root=False, color=None, move=None):
         
         self.state = state
         self.parent = parent
@@ -18,12 +20,15 @@ class node:
         self.wins = 0
         self.simulations = 0
         self.is_terminal = False
+        self.move = move
+        self.depth = 0
 
         if not self.is_root:
             if self.parent == "white":
                 self.color == "black"
             else:
                 self.color == "white"
+            self.depth = self.parent.depth + 1
 
         board = chess.Board(state)
         if board.is_game_over():
@@ -49,15 +54,17 @@ class node:
             new_state=board.copy()
             new_state.push_san(mv)
             new_state = new_state.fen()
-            children.append(node(new_state, self))
+            children.append(node(new_state, self, move=mv))
+            random.shuffle(children)
             self.children = children
 
 
 class MonteCarlo():
-    def __init__(self, ucn_constant):
+    def __init__(self, ucb_constant):
         print("Monte-Carlo placeholder")
         self.root = None
-        self.ucb_constant = ucn_constant
+        self.ucb_constant = ucb_constant
+        self.epsilon = .9
 
 
     def search(self, starting_state, time_limit, color):
@@ -67,9 +74,13 @@ class MonteCarlo():
         #loop until time expires
         count = 0
         end = time.time()+time_limit
+        depth_plot = []
         while time.time()<=end:
+            if count % 10 == 0:
+                self.epsilon -= .1
             #select a leaf
             leaf = self.select_child(self.root)
+            depth_plot.append(leaf.depth)
             #simulate until an end condition
             sim_result = self.simulation(leaf)
             #back propogate the results up to the orginal starting node
@@ -79,17 +90,23 @@ class MonteCarlo():
         #once time is up, return the the child node with the greatest evaluation based on the upper confidence bound
         for child in self.root.children:
             print(f"{child.wins} out of {child.simulations} for {child.state}") #max(child.simulations for child in self.root.children)
-        
+        mx = -100
+        best_move = None
+        for child in self.root.children:
+            if child.simulations > mx:
+                best_move = child.move
+                mx = child.simulations
+        print(f"The current best move is {best_move}")
+        plt.plot(depth_plot)
+        plt.show()
+
     def select_child(self, node):
         #finds a leaf node - will first pick unvisited nodes, then switch to to highest UCB value 
         while self.fully_epanded(node):
             for child in node.children:
                 child.calc_ucb(self.ucb_constant)
-            max_ucb = -100
-            for child in node.children:
-                if child.ucb > max_ucb:
-                    max_node = child
-            node = max_node
+            node = max(node.children, key=attrgetter('ucb'))
+
         if node.is_terminal:
         #if a terminal node, return self    
             return node
@@ -98,17 +115,34 @@ class MonteCarlo():
             if not child.visited:
                 child.visited = True
                 return child
-    
+
     def simulation(self, node):
         #simulate the game by choosing moves at random
         #Opponents moves are also chosen randomly
-        while not node.is_terminal:
+        #Simulation only allo
+        count = 0
+        while not node.is_terminal and count < 40:
             if node.children == []:
                 node.expand_children()
-            node = random.choice(node.children)
+            if random.random() < self.epsilon:
+                node = random.choice(node.children)
+            else:
+                node = max(node.children, key=attrgetter('ucb'))
+            count += 1
+        #get results of
         board = chess.Board(node.state)
-        result = board.result()
+        if board.is_game_over():
+            result = board.result()
+        else:
+            result = self.evaluate_board_state(node)
         return ((node, result))
+
+
+    def evaluate_board_state(self, node):
+    #need board state evaluation function
+    #not implemented
+        return "1/2-1/2"
+
 
     def backprop(self, leaf, result):
         ##not sure how to implement, don't know yet how to interpret results from game
@@ -137,10 +171,6 @@ class MonteCarlo():
             else:
                 node.wins += -1
 
-        
-        
-
-
     def fully_epanded(self, node):
         #check if all chidlren have been visited
         if node.children == None:
@@ -151,7 +181,7 @@ class MonteCarlo():
         return True
 
 if __name__== '__main__':
-    agent = MonteCarlo(10)
+    agent = MonteCarlo(1)
 
-    agent.search("2kr4/2p5/8/8/8/8/5P2/4RK2 w - - 0 1", 30, "white")
+    agent.search('4k3/8/8/8/8/8/8/1R1K1R2 w - - 6 4',60,"white")
 
