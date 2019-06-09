424a6a0659506d8b65f06e736b865e8b042ca8f0
Ray Emory <remory@summitsemi.com>
2019-06-09 12:37:19 -0700

    Fixed AB pruning & eval statement

--- minimax.py
+++ minimax.py
@@ -23,8 +23,8 @@ class MiniMax():
         depth = 0         #Init depth
         limit = self.limit #Depth limit
         best = ''         #Best move evaluated.
-        alpha = -1000
-        beta = 1000
+        alpha = 0
+        beta = 0
 
         best, value = self.evaluate(depth, limit, True, alpha, beta) #Calls evaluate function and returns best move.
         return best
@@ -49,7 +49,9 @@ class MiniMax():
             value = -1000;
             for move in moves:
                 self.state.push(move);
-                move_val = self.checkMove(self.state, self.state.turn) #Evaluates individual move. checkMove() does not exist.
+                move_val = self.checkMove(self.state, self.state.turn, depth+1, player) #Evaluates individual move. checkMove() does not exist.
+                #if move_val > 200:
+                    #print("{} at {}".format(move_val, depth+1))
                 catch, total = self.evaluate(depth+1, limit, False, alpha, beta)
                 move_val += total
                 alpha = max(move_val, alpha)
@@ -57,17 +59,17 @@ class MiniMax():
                     value = move_val
                     best = move
                 self.state.pop()
-                if beta <= alpha:
+                if beta > alpha:
                     break
             return best, value
 
-                #INCOMPLETE
+                #DONT UNDO PAST HERE
 
         else:
             value = 1000;
             for move in moves:
                 self.state.push(move)
-                move_val = self.checkMove(self.state, self.state.turn)
+                move_val = self.checkMove(self.state, self.state.turn, depth+1, player)
                 move_val = -move_val
                 catch, total = self.evaluate(depth+1, limit, True, alpha, beta)
                 move_val += total
@@ -76,11 +78,11 @@ class MiniMax():
                     value = move_val
                     catch = move
                 self.state.pop()
-                if beta <= alpha:
+                if beta < alpha:
                     break
             return catch, value
 
-    def checkMove(self, state, turn):
+    def checkMove(self, state, turn, depth, play):
         """
         <state> = str(board)
         <turn> = board.turn
@@ -103,22 +105,46 @@ class MiniMax():
         if state.is_game_over(): #Checking for draw or checkmate
             result = state.result()
             [white,black] = result.split('-')
+            #print(depth)
             if white == black: #Game is a draw
                 return 0
-            if white == '1': #White won
-                if turn: #And is white's turn/state
-                    return whiteDict['K'] * 3
-                else: #And is black's turn/state
-                    return whiteDict['k'] * 3
-            else: #Black won
-                if not turn: #And is black's turn/state
-                    return -whiteDict['k'] * 3
-                else: #And is white's turn/state
-                    return -whiteDict['K'] * 3
+            elif white == '1': #White won
+                if not play:
+                    return 270+depth
+                elif play:
+                    return 270-depth
+            elif black == '1': #Black won
+                if not play:
+                    return 270+depth
+                elif play:
+                    return 270-depth
+            else:
+                print("Weird state")
         for char in str(state):
             points += whiteDict.get(char, 0)
-        if turn:
+        if turn and play:
+            points *= -1
+        elif not turn and not play:
             points *= -1
         #print(points)
         return points
           #INCOMPLETE
+
+def main():
+    board1 = chess.Board('2k5/R6R/8/8/8/8/8/3K4 w - - 0 1')
+    board2 = chess.Board('2k5/8/8/8/8/8/r6r/3K4 b - - 0 1')
+    board3 = chess.Board('2k5/8/R6r/8/8/8/8/3K4 w - - 0 1')
+    board4 = chess.Board('2k5/8/R6r/8/8/8/8/3K4 b - - 0 1')
+    
+    mm = MiniMax(state=chess.Board(), limit=5)
+    print(board1)
+    print(mm.getMove(board1.copy()))
+    print(board2)
+    print(mm.getMove(board2.copy()))
+    print(board3)
+    print(mm.getMove(board3.copy()))
+    print(board4)
+    print(mm.getMove(board4.copy()))    
+
+if __name__ == '__main__':
+    main()
